ec8ee3c974bc25b636a0d2d7bf6d42dc4e458c61
iamgromov <iamgromov@gmail.com>
2023-05-01 13:00:16 +0300

    first commit keysolo

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -25,6 +25,16 @@ class Game {
       При неправильном вводе символа - this.fail();
       DOM-элемент текущего символа находится в свойстве this.currentSymbol.
      */
+
+    document.addEventListener('keydown', (event) => {
+      if (event.key !== 'Shift' && event.key !== 'CapsLock') {
+        if (event.key.toLowerCase() === this.currentSymbol.textContent) {
+          this.success();
+        } else {
+          this.fail();
+        }
+      }
+    });
   }
 
   success() {
